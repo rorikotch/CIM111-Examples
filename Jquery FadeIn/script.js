8d1b815f262e78872339fa5456bed6e3e0c1240c
Rori Kotch <r.kotch1@umiami.edu>
2018-03-20 20:08:50 -0400

    Added two jQuery examples

---
+++ Jquery FadeIn/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("box1").fadeIn();
+        $("#box2").fadeIn("slow");
+        $("#box3").fadeIn("slow");
+        $("#box4").fadeIn("slow");
+        $("#box5").fadeIn("2000");
+        $("#box6").fadeIn("3000");
+    });
+    $("button").click(function(){
+        $("box1").fadeOut();
+        $("#box2").fadeOut("slow");
+        $("#box3").fadeOut("slow");
+        $("#box4").fadeOut("slow");
+        $("#box5").fadeOut("2000");
+        $("#box6").fadeOut("3000");
+    });
+});
