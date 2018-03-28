09b4d38073796b60a814e94d8aeacf8152288829
Rori Kotch <r.kotch1@umiami.edu>
2018-03-28 12:23:56 -0400

    jquery how to assignment

---
+++ Jquery How-To Example/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $(".imageone").hide();
+    });
+
+    $("#show").click(function(){
+        $(".imageone").show();
+    });
+
+    $(".screen").hover(function(){
+        $(".screen").fadeOut(1000);
+  });
+
+  $(".imghover").hover(function(){
+      $(".screen").fadeIn(1000);
+});
+
+$(".moreinfo").hover(function(){
+    $("a",).css("background-color", "blue");
+    $("a",).css("color", "white");
+});
+
+$("#clickhere").click(function(){
+    $(".moreinfo").show();
+});
+
+$("#replacetext").hover(function(){
+$("#replace").html("<p>Repeat the process and enjoy!</p>");
+});
+
+
+
+
+
+});
